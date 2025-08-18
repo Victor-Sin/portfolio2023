7b31598c2e5141becb6b775260c98fced05be1d8
Victor-Sin <80609577+Victor-Sin@users.noreply.github.com>
2025-08-18 18:12:37 +0200

    Delete src directory

--- src/Experience/Utils/Helper.js
+++
@@ -1,5 +0,0 @@
-export const extendFragmentShader = (originalShader, customCode) => {
-    const hook = 'INJECTION_POINT';
-    const index = originalShader.indexOf(hook);
-    return originalShader.slice(0, index) + customCode + originalShader.slice(index + hook.length);
-};
